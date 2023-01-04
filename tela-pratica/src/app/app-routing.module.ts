978bc7ae910d744e267f3892112b542be9e71ede
João Longo <joao.longgo@gmail.com>
2023-01-04 14:58:13 -0300

    ajustes gerais em todas as telas

--- tela-pratica/src/app/app-routing.module.ts
+++ tela-pratica/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { unidadeNegocio } from './unidade-negocio/unidade-negocio.component';
+import { DetFrenteEstrategicaComponent } from './det-frente-estrategica/det-frente-estrategica.component';
+import { FrenteEstrategicaComponent } from './frente-estrategica/frente-estrategica.component';
+import { HomeComponent } from './home/home.component';
+import { cicloComponent } from './ciclo/ciclo.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'ciclo', component: cicloComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'frente-estrategica', component: FrenteEstrategicaComponent},
+  {path: 'detalhamento-frente-estrategica', component: DetFrenteEstrategicaComponent},
+  {path: 'unidade-negocio', component: unidadeNegocio}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
